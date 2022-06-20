f729e83d176ef83ef596539f0446ea3afc9d6fcd
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-06-20 16:41:02 -0300

    Preparando revisão de código

--- middwares/validateSchemaMiddwares.js
+++
@@ -1,9 +0,0 @@
-export default function validSchema(sch){
-    return (req, res, next) => {
-        const {error} = sch.validate(req.body, {abortEarly: false});
-        if(error){
-            return res.status(422).send(error.details.map(detail => detail.message));
-        }
-        next();
-    }
-}
